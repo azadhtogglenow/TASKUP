61aed44d95f4bdf8bc94f2876d94b46d864f6c69
Azadh <azadhroshan777@gmail.com>
2026-08-19 17:22:18 +0530

    TASK13

--- src/controllers/search.controller.ts
+++ src/controllers/search.controller.ts
@@ -1,4 +1,3 @@
-//C:\Users\Azadh-desktop\OneDrive\Desktop\document-api\src\controllers\search.controller.ts
 import { Request, Response } from "express";
 import { EmbeddingService } from "../services/embedding-service.js";
 import { VectorService } from "../services/vector-service.js";
